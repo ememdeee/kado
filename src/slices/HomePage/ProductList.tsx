310e1ce818883101d888d474ca2761180219aa00
ememdeee <basurah.muhammad.mb@gmail.com>
2024-05-19 23:29:02 +0700

    backend done

--- src/slices/HomePage/ProductList.tsx
+++ src/slices/HomePage/ProductList.tsx
@@ -7,62 +7,60 @@ import "@/app/globals.css";
 import Link from "next/link";
 import TextHoverable from "@/component/TextHoverable";
 
-
 type ProductListProps = {
   products: Content.ProductDocument[];
 };
 
-export default function ProductList({
-    products,
-  }: ProductListProps) {
+export default function ProductList({ products }: ProductListProps) {
+  // Extract and deduplicate tags
+  const allTags = products.flatMap(product => product.tags);
+  const uniqueTags = Array.from(new Set(allTags));
 
-    // Extract and deduplicate tags
-    const allTags = products.flatMap(product => product.tags);
-    const uniqueTags = Array.from(new Set(allTags));
+  // const selectedTag: string[] = [];
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const handleTagClick = (tag: string) => {
+    setSelectedTags(prevSelectedTags => {
+      if (prevSelectedTags.includes(tag)) {
+        // Remove tag if it's already selected
+        return prevSelectedTags.filter(t => t !== tag);
+      } else {
+        // Add tag if it's not selected
+        return [...prevSelectedTags, tag];
+      }
+    });
+  };
 
-    // const selectedTag: string[] = [];
-    const [selectedTags, setSelectedTags] = useState<string[]>([]);
-    const handleTagClick = (tag: string) => {
-        setSelectedTags((prevSelectedTags) => {
-          if (prevSelectedTags.includes(tag)) {
-            // Remove tag if it's already selected
-            return prevSelectedTags.filter((t) => t !== tag);
-          } else {
-            // Add tag if it's not selected
-            return [...prevSelectedTags, tag];
-          }
-        })}
-    const reset = () => {
-        setSelectedTags([])
-    }
-  
-    return (
-      <>
-        <p className="mt-3 mb-3">List of tag available:</p>
-        {uniqueTags.map((tag, index) => (
-        <span key={index} onClick={() => handleTagClick(tag)} className="inline-block">
-            <TextHoverable label={tag} className="ml-1"/>
+  const reset = () => {
+    setSelectedTags([]);
+  };
+
+  return (
+    <>
+      <p className="mt-3 mb-3">List of tag available:</p>
+      {uniqueTags.map(tag => (
+        <span key={tag} onClick={() => handleTagClick(tag)} className="inline-block">
+          <TextHoverable label={tag} className="ml-1" />
         </span>
-        ))}
+      ))}
 
-        {products
+      {products
         .filter(product => selectedTags.every(tag => product.tags.includes(tag)))
         .map(product => (
-            <h2 key={product.id}>
-                <Link href={product.uid} passHref>
-                {product.data.title} - 
-                {product.tags.map(tag => (
+          <h2 key={product.id}>
+            <Link href={product.uid} passHref>
+              {product.data.title} - 
+              {product.tags.map(tag => (
                 <span key={tag}> {tag} </span>
-                ))}
-                </Link>
-            </h2>
-            ))}
-        <p className="mt-4">
-            Selected Tag: {selectedTags.map((tag, index) =>(
-                <span key={index}>-{tag}</span>
-            ))}
-        </p>
-        <p onClick={() => reset()}>Reset Filter</p>
-      </>
-    );
-  }
+              ))}
+            </Link>
+          </h2>
+        ))}
+      <p className="mt-4">
+        Selected Tag: {selectedTags.map(tag => (
+          <span key={tag}>-{tag}</span>
+        ))}
+      </p>
+      <p onClick={reset}>Reset Filter</p>
+    </>
+  );
+}
