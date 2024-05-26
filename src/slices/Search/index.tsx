a3f6d20e4ab3f65b827bf4149b3aa0f6bcc5662d
ememdeee <basurah.muhammad.mb@gmail.com>
2024-05-26 09:18:41 +0700

    changing to form and add search page

--- src/slices/Search/index.tsx
+++ src/slices/Search/index.tsx
@@ -8,31 +8,44 @@ import Heading from "@/component/Heading";
 /**
  * Props for `GalleryIndex`.
  */
-export type GalleryIndexProps = SliceComponentProps<Content.GalleryIndexSlice>;
+export type GalleryIndexProps = SliceComponentProps<Content.GalleryIndexSlice> & {
+  tags: string[];
+};
 
 /**
  * Component for "GalleryIndex" Slices.
  */
-const GalleryIndex = async ({ slice }: GalleryIndexProps): Promise <JSX.Element> => {
-  console.log("currentUrl: "); //dapeting current querry string
-  const tags = ["makanan", "komputer"]; //ini harusnya dari querry string ex: /search?q=makanan+komputer
-  const client = createClient()
-  const products = await client.getAllByType('product')
-  // const products = await client.getAllByEveryTag(tags)
+const GalleryIndex = async ({ slice, tags }: GalleryIndexProps): Promise<JSX.Element> => {
+  const client = createClient();
+  const products = await client.getAllByType('product');
+  // const products = await client.getAllByEveryTag(tags);
 
   return (
-    <>
-      <Bounded
-        data-slice-type={slice.slice_type}
-        data-slice-variation={slice.variation}>
-          <Heading>{slice.primary.heading}</Heading>
-          <PrismicRichText field={slice.primary.description} />
-          <ProductList products={products} className="mt-4" />
-          <p>Query String List list</p>
-          {/* map right here: */}
-      </Bounded>
-    </>
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}>
+      <Heading>{slice.primary.heading}</Heading>
+      <PrismicRichText field={slice.primary.description} />
+      <ProductList products={products} className="mt-4" />
+      <p>Query String List:</p>
+      <p>{tags}</p>
+    </Bounded>
   );
 };
 
 export default GalleryIndex;
+
+export const getServerSideProps = async (context: any) => {
+  const { q } = context.query;
+  const tags = typeof q === 'string' ? q.split("+") : [];
+
+  // Debugging output
+  console.log('Query parameter:', q);
+  console.log('Tags array:', tags);
+
+  return {
+    props: {
+      tags
+    }
+  };
+};
