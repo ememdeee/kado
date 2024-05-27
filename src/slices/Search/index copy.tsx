63f562a668bdee2f9058618032d1b37e74f62c9a
ememdeee <basurah.muhammad.mb@gmail.com>
2024-05-27 12:58:52 +0700

    UI Fixes

--- src/slices/Search/index copy.tsx
+++ src/slices/Search/index copy.tsx
@@ -8,20 +8,17 @@ import Heading from "@/component/Heading";
 /**
  * Props for `GalleryIndex`.
  */
-// export type GalleryIndexProps = SliceComponentProps<Content.GalleryIndexSlice> & {
-//   context: {
-//     tags: string[];
-//   };
-// };
-export type GalleryIndexProps = SliceComponentProps<Content.GalleryIndexSlice>;
+export type GalleryIndexProps = SliceComponentProps<Content.GalleryIndexSlice> & {
+  context: {
+    tags: string[];
+  };
+};
 
 /**
  * Component for "GalleryIndex" Slices.
  */
-// const GalleryIndex = async ({ slice, context }: GalleryIndexProps): Promise <JSX.Element> => {
-const GalleryIndex = async ({ slice }: GalleryIndexProps): Promise <JSX.Element> => {
-  // const tags = context.tags; //ini dari querrystring yg di kriim dari index
-  const tags = ["makanan"];
+const GalleryIndex = async ({ slice, context }: GalleryIndexProps): Promise <JSX.Element> => {
+  const tags = context.tags; //ini dari querrystring yg di kriim dari index
   const client = createClient()
   const documents = await client.getAllByEveryTag(tags) //or getAllByType('product') to get all
   const products = documents.filter(doc => doc.type === "product");
