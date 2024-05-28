d86074c0784d58d10fa27b25a1ba0557e64370a4
ememdeee <basurah.muhammad.mb@gmail.com>
2024-05-28 12:37:18 +0700

    UI Fixes

--- src/slices/Search/ProductList.tsx
+++ src/slices/Search/ProductList.tsx
@@ -9,6 +9,7 @@ import TextHoverable from "@/component/TextHoverable";
 import Heading from "@/component/Heading";
 import { PrismicImage, PrismicRichText } from "@prismicio/react";
 import Button from "@/component/Button";
+import { useSearchParams } from "next/navigation";
 
 type ProductListProps = {
   products: Content.ProductDocument[];
@@ -16,11 +17,21 @@ type ProductListProps = {
 };
 
 export default function ProductList({ products, className }: ProductListProps) {
+  let tags: string[] = []; //ini dari querrystring yg di kriim dari index
+  const searchParams = useSearchParams();
+  const q = searchParams.get("q");
+  console.log(q)
+  if (typeof q === 'string') {
+    tags = q.split(' ');
+    console.log("Query: ", tags);
+  }
 
   return (
     <div className={clsx("", className)}>
       <div className="w-fit mx-auto grid gap-y-20 gap-x-14 mt-10 mb-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
-        {products.map(product => (
+        {products
+        .filter(product => tags.every(tag => product.tags.includes(tag)))
+        .map(product => (
             <Link href={product.uid} passHref key={product.id} className="max-w-72 w-full">
               <div className="bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
                 <PrismicImage field={product.data.mainimage} className="h-80 object-cover rounded-t-xl" />
