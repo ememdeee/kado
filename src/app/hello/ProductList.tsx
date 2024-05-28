c52b2a6d93bf67229379a8d6163ddce3d77b98c5
ememdeee <basurah.muhammad.mb@gmail.com>
2024-05-28 12:21:04 +0700

    UI Fixes

---
+++ src/app/hello/ProductList.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useState } from "react";
+import clsx from "clsx";
+import { Content } from "@prismicio/client";
+import "@/app/globals.css";
+import Link from "next/link";
+import TextHoverable from "@/component/TextHoverable";
+import Heading from "@/component/Heading";
+import { PrismicImage, PrismicRichText } from "@prismicio/react";
+import Button from "@/component/Button";
+
+type ProductListProps = {
+  products: Content.ProductDocument[];
+  className?: string;
+};
+
+export default function ProductList({ products, className }: ProductListProps) {
+
+  return (
+    <div className={clsx("", className)}>
+      <div className="w-fit mx-auto grid gap-y-20 gap-x-14 mt-10 mb-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
+        {products.map(product => (
+            <Link href={product.uid} passHref key={product.id} className="max-w-72 w-full">
+              <div className="bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+                <PrismicImage field={product.data.mainimage} className="h-80 object-cover rounded-t-xl" />
+                <div className="px-4 py-3">
+                  <Heading as="h2" size="ss" className="mb-2">
+                    {product.data.title}
+                  </Heading>
+                  <span className='text-xl font-bold'>Rp. {product.data.price}</span>
+                  <TextHoverable label="Details" className="mt-5" />
+                </div>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
+}
--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,17 +1,35 @@
 // app/hello/page.tsx 
 
+import { Content } from "@prismicio/client";
+import {createClient } from "@/prismicio";
+import ProductList from "./ProductList";
+
+
 interface HomeProps {
   searchParams: {
     q?: string;
   };
 }
 
-export default function Home({ searchParams }: HomeProps) {
+export default async function Home({ searchParams }: HomeProps) {
   console.log(searchParams)
   const query = searchParams.q;
+  let tags: string[] = [];
+  if (typeof query === 'string') {
+    tags = query.split(' ');
+  }
+  console.log(tags)
+
+  const client = createClient();
+  const documents = await client.getAllByType('product')
+  // const documents = await client.getAllByEveryTag(tags) //or getAllByType('product') to get all
+  const products = documents.filter(doc => doc.type === "product");
+
   return (
     <div className=''>
       <h1 className='font-bold text-center text-3xl'>Search Query:  {query}</h1>
+      <h2 className='font-bold text-center text-3xl'>Tags:  {tags.join(", ")}</h2>
+      <ProductList products={products as Content.ProductDocument[]} className="mt-4" />
     </div>
   );
 }
