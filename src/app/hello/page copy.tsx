e26c3d551ad70d63551a5c0ea4a1d80215d5e585
ememdeee <basurah.muhammad.mb@gmail.com>
2024-05-28 23:58:28 +0700

    UI Fixes

---
+++ src/app/hello/page copy.tsx
@@ -0,0 +1,35 @@
+// app/hello/page.tsx 
+
+import { Content } from "@prismicio/client";
+import {createClient } from "@/prismicio";
+import ProductList from "./ProductList";
+
+
+interface HomeProps {
+  searchParams: {
+    q?: string;
+  };
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  console.log(searchParams)
+  const query = searchParams.q;
+  let tags: string[] = [];
+  if (typeof query === 'string') {
+    tags = query.split(' ');
+  }
+  console.log(tags)
+
+  const client = createClient(); //errornya disini, harus manual kayknya?
+  // const documents = await client.getAllByType('product')
+  const documents = await client.getAllByEveryTag(["makanan"]) //or getAllByType('product') to get all
+  const products = documents.filter(doc => doc.type === "product");
+
+  return (
+    <div className=''>
+      <h1 className='font-bold text-center text-3xl'>Search Query:  {query}</h1>
+      <h2 className='font-bold text-center text-3xl'>Tags:  {tags.join(", ")}</h2>
+      <ProductList products={products as Content.ProductDocument[]} className="mt-4" />
+    </div>
+  );
+}
--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,9 +1,8 @@
-// app/hello/page.tsx 
+// app/hello/page.tsx
 
 import { Content } from "@prismicio/client";
-import {createClient } from "@/prismicio";
+import { createClient } from "@/prismicio";
 import ProductList from "./ProductList";
-
 
 interface HomeProps {
   searchParams: {
@@ -11,25 +10,26 @@ interface HomeProps {
   };
 }
 
-export default async function Home({ searchParams }: HomeProps) {
-  console.log(searchParams)
+const hello = async ({ searchParams }: HomeProps): Promise<JSX.Element> => {
+  console.log(searchParams);
   const query = searchParams.q;
   let tags: string[] = [];
   if (typeof query === 'string') {
     tags = query.split(' ');
   }
-  console.log(tags)
+  console.log(tags);
 
-  const client = createClient(); //errornya disini, harus manual kayknya?
-  // const documents = await client.getAllByType('product')
-  const documents = await client.getAllByEveryTag(["makanan"]) //or getAllByType('product') to get all
+  const client = createClient();
+  const documents = await client.getAllByEveryTag(tags.length > 0 ? tags : [""]);
   const products = documents.filter(doc => doc.type === "product");
 
   return (
     <div className=''>
-      <h1 className='font-bold text-center text-3xl'>Search Query:  {query}</h1>
-      <h2 className='font-bold text-center text-3xl'>Tags:  {tags.join(", ")}</h2>
+      <h1 className='font-bold text-center text-3xl'>Search Query: {query}</h1>
+      <h2 className='font-bold text-center text-3xl'>Tags: {tags.join(", ")}</h2>
       <ProductList products={products as Content.ProductDocument[]} className="mt-4" />
     </div>
   );
 }
+
+export default hello;
