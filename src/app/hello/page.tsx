919b1a8476887f04c7b6d3c8bcc06ce240079b95
ememdeee <basurah.muhammad.mb@gmail.com>
2024-05-29 06:44:22 +0700

    UI Fixes

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,6 +1,9 @@
+// app/hello/page.tsx 
+
 import { Content } from "@prismicio/client";
-import { createClient } from "@/prismicio";
+import {createClient } from "@/prismicio";
 import ProductList from "./ProductList";
+
 
 interface HomeProps {
   searchParams: {
@@ -9,30 +12,23 @@ interface HomeProps {
 }
 
 export default async function Home({ searchParams }: HomeProps) {
-  console.log(searchParams);
+  console.log(searchParams)
   const query = searchParams.q;
   let tags: string[] = [];
   if (typeof query === 'string') {
     tags = query.split(' ');
   }
-  console.log(tags);
+  console.log(tags)
 
-  const client = createClient();
-  let documents = [];
-
-  // Fetch documents based on tags
-  if (tags.length > 0) {
-    documents = await client.getAllByEveryTag(tags);
-  } else {
-    documents = await client.getAllByType('product');
-  }
-
+  const client = createClient(); //errornya disini, harus manual kayknya?
+  // const documents = await client.getAllByType('product')
+  const documents = await client.getAllByEveryTag(["makanan"]) //or getAllByType('product') to get all
   const products = documents.filter(doc => doc.type === "product");
 
   return (
     <div className=''>
-      <h1 className='font-bold text-center text-3xl'>Search Query: {query}</h1>
-      <h2 className='font-bold text-center text-3xl'>Tags: {tags.join(", ")}</h2>
+      <h1 className='font-bold text-center text-3xl'>Search Query:  {query}</h1>
+      <h2 className='font-bold text-center text-3xl'>Tags:  {tags.join(", ")}</h2>
       <ProductList products={products as Content.ProductDocument[]} className="mt-4" />
     </div>
   );
