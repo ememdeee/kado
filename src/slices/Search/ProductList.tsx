deae04eb684bd99e790799048e5c414be3baa1c7
ememdeee <basurah.muhammad.mb@gmail.com>
2024-10-14 05:59:54 +0700

    animation homepage, bg in product, bugs fixes

--- src/slices/Search/ProductList.tsx
+++ src/slices/Search/ProductList.tsx
@@ -13,16 +13,15 @@ type ProductListProps = {
 };
 
 export default function ProductList({ products, className }: ProductListProps) {
-  let tags: string[] = []; //ini dari querrystring yg di kriim dari index
   const searchParams = useSearchParams();
   const q = searchParams.get("q");
-  console.log(q)
 
+  let tags: string[] = [];
   let minPrice = 0;
   let maxPrice = 999999999;
 
-  if (typeof q === 'string') {
-    tags = q.split(' ');
+  if (typeof q === 'string' && q.trim() !== '') {
+    tags = q.split(' ').filter(tag => tag !== '');
     // Find and extract max,min price, removing it from the tags array
     const maxIndex = tags.findIndex(tag => tag.startsWith('Max_'));
     if (maxIndex !== -1) {
@@ -32,18 +31,19 @@ export default function ProductList({ products, className }: ProductListProps) {
     if (minIndex !== -1) {
       minPrice = parseInt(tags.splice(minIndex, 1)[0].replace('Min_', ''), 10);
     }
-
-    console.log("Query: ", tags);
-    console.log("Min: ", minPrice);
-    console.log("Max: ", maxPrice);
   }
+
+  console.log("Query: ", tags);
+  console.log("Query.length: ", tags.length);
+  console.log("Min: ", minPrice);
+  console.log("Max: ", maxPrice);
 
   return (
     <div className={clsx("", className)}>
       <div className="w-fit mx-auto grid gap-y-4 gap-x-4 mt-10 mb-5 grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 justify-items-center">
         {products
           .filter(product => 
-            tags.every(tag => product.tags.includes(tag)) &&
+            (tags.length === 0 || tags.every(tag => product.tags.includes(tag))) &&
             (product.data.price_new !== null && 
             product.data.price_new >= minPrice &&
             product.data.price_new <= maxPrice)
